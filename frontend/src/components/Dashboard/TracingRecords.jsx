0e58cccb69454f7bb547f63767ec71ef67976a31
Monirul Islam <Monirul Islam>
2022-10-01 02:08:30 +0600

    creaete api and integrated 1. create api to get records 2. integrated api with the recoreds

--- frontend/src/components/Dashboard/TracingRecords.jsx
+++ frontend/src/components/Dashboard/TracingRecords.jsx
@@ -6,53 +6,66 @@ import {
   Th,
   Thead,
   Tr,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 
-import React, { useState } from "react";
-
+import { useQuery } from '@tanstack/react-query';
+import React, { useEffect, useState } from 'react';
 const FetchedData = [
   {
-    infectant1: "Alice",
-    infectant2: " Bernard",
-    time: "2020/12/11 09:12 AM",
+    infectant1: 'Alice',
+    infectant2: ' Bernard',
+    time: '2020/12/11 09:12 AM',
   },
   {
-    infectant1: "David",
-    infectant2: "Claria",
-    time: "2022/05/16 02:15 AM",
+    infectant1: 'David',
+    infectant2: 'Claria',
+    time: '2022/05/16 02:15 AM',
   },
   {
-    infectant1: "Claria",
-    infectant2: "David",
-    time: "2021/07/13 09:12 PM",
+    infectant1: 'Claria',
+    infectant2: 'David',
+    time: '2021/07/13 09:12 PM',
   },
   {
-    infectant1: "Claria",
-    infectant2: "Emily",
-    time: "2020/03/18 04:42 AM",
+    infectant1: 'Claria',
+    infectant2: 'Emily',
+    time: '2020/03/18 04:42 AM',
   },
   {
-    infectant1: "Sandy",
-    infectant2: "Bernard",
-    time: "2021/01/14 09:15 AM",
+    infectant1: 'Sandy',
+    infectant2: 'Bernard',
+    time: '2021/01/14 09:15 AM',
   },
 ];
 export default function TracingRecords() {
-  const [data, setData] = useState(FetchedData);
+  const { data: records, isSuccess } = useQuery(['records'], async () => {
+    const res = await fetch('http://localhost:4000/tracing/records');
+    const data = await res.json();
+    return data;
+  });
+  const [data, setData] = useState([]);
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    if (isSuccess) {
+      setData(records);
+    }
+
+    return () => {};
+  }, [isSuccess, records]);
+
   const getSortedData = (sortBy) => {
-    let dataToSort = data;
-    dataToSort.sort((a, b) => {
+    if (!isSuccess) return [];
+    const dataToSort = records.slice().sort((a, b) => {
       let aVal = a[sortBy];
       let bVal = b[sortBy];
       switch (typeof aVal) {
-        case "string":
+        case 'string':
           return aVal.localeCompare(bVal);
-        case "number":
+        case 'number':
           return aVal - bVal;
         default:
-          throw new Error("Unsupported value to sort by");
+          throw new Error('Unsupported value to sort by');
       }
     });
     setCount(count + 1);
@@ -60,6 +73,7 @@ export default function TracingRecords() {
   };
 
   const filterTable = (e) => {
+    if (!isSuccess) return [];
     let filterFunc = (item) => {
       if (
         item.infectant1.indexOf(e) >= 0 ||
@@ -76,47 +90,46 @@ export default function TracingRecords() {
     <>
       <div
         style={{
-          display: "flex",
-          justifyContent: "flex-end",
+          display: 'flex',
+          justifyContent: 'flex-end',
           marginBottom: 20,
         }}
       >
-
         <p>Search for:</p>
         <input
-          type="text"
+          type='text'
           onChange={(e) => filterTable(e.target.value)}
-          placeholder="Search for Infectants and Time..."
+          placeholder='Search for Infectants and Time...'
           style={{
-            background: "rgba(0,0,0,0.2)",
+            background: 'rgba(0,0,0,0.2)',
             borderRadius: 5,
             marginLeft: 20,
             paddingLeft: 10,
             paddingRight: 10,
             width: 350,
-            height: 30
+            height: 30,
           }}
         />
       </div>
       <TableContainer>
-        <Table variant="simple" colorScheme={"facebook"}>
+        <Table variant='simple' colorScheme={'facebook'}>
           <Thead>
             <Tr>
               <Th
-                onClick={() => getSortedData("infectant1")}
-                style={{ cursor: "pointer" }}
+                onClick={() => getSortedData('infectant1')}
+                style={{ cursor: 'pointer' }}
               >
                 Infectant 1
               </Th>
               <Th
-                onClick={() => getSortedData("infectant2")}
-                style={{ cursor: "pointer" }}
+                onClick={() => getSortedData('infectant2')}
+                style={{ cursor: 'pointer' }}
               >
                 Infectant 2
               </Th>
               <Th
-                onClick={() => getSortedData("time")}
-                style={{ cursor: "pointer" }}
+                onClick={() => getSortedData('time')}
+                style={{ cursor: 'pointer' }}
               >
                 Time of Close Contact
               </Th>
@@ -125,8 +138,8 @@ export default function TracingRecords() {
           <Tbody>
             {data.map((item, i) => (
               <Tr key={i.toString()}>
-                <Td>{item.infectant1}</Td>
-                <Td>{item.infectant2}</Td>
+                <Td>{item.closecontactuid}</Td>
+                <Td>{item.uid}</Td>
                 <Td>{item.time}</Td>
               </Tr>
             ))}
